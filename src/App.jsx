de6c62cd443d587bb84f75f668d28d47b00cba5e
Liisa Davydov <liisamuttonen@gmail.com>
2024-11-11 11:19:25 +0200

    Add menu hamburger from MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
 import Customerlist from "./components/Customerlist";
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import CssBaseline from '@mui/material/CssBaseline'
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
   return (
     <Container maxwidth="xl">
-          <CssBaseline />
-    <AppBar position='static'>
-      <Toolbar>
-        <Typography variant = 'h6'>Personal Trainer App</Typography>
-      </Toolbar>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">Personal Trainer App</Typography>
+        </Toolbar>
       </AppBar>
       <Customerlist />
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
