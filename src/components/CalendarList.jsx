a2a0db69be9f8b0296f2c47d2f78b30137fd4643
Liisa Davydov <liisamuttonen@gmail.com>
2024-11-30 01:20:31 +0200

    Add Bar Chart by using rechart github repo and lodash

--- src/components/CalendarList.jsx
+++ src/components/CalendarList.jsx
@@ -1,7 +1,77 @@
-import React from 'react';
+import { useState, useEffect } from 'react'; // Import useState and useEffect
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import Snackbar from '@mui/material/Snackbar'; // Import Snackbar
+import { fetchTrainings } from '../ptapi';
+
+const localizer = momentLocalizer(moment);
 
 function CalendarList() {
-  return <div>Calendar List Component</div>;
+  const [events, setEvents] = useState([]);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+
+  useEffect(() => {
+    const fetchCalendarData = () => {
+      fetchTrainings()
+      .then((trainings) => {
+        const formattedEvents = trainings.map((training) => {
+          return {
+            title: training.activity,
+            start: new Date(training.date),
+            end: new Date(new Date(training.date).getTime() + training.duration * 60000),
+            customer: `${training.customer?.firstname} ${training.customer?.lastname}`.trim(),
+            description: `Training with ${training.customer?.firstname} ${training.customer?.lastname}`.trim(),
+          };
+        });
+        setEvents(formattedEvents);
+      })
+      .catch((err) => setError(err.message))
+      .finally(() => setLoading(false));
+  };
+  
+    fetchCalendarData(); 
+  }, []);
+
+  const handleSnackbarClose = () => {
+    setOpenSnackbar(false); 
+  };
+
+  if (loading) {
+    return <div>Loading data...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={events} 
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        eventPropGetter={(event) => ({
+          style: {
+            backgroundColor: event.activity === 'Yoga' ? '#ff8fab' : '#3f51b5', // Set color based on activity type (example)
+            color: '#fff', // White text color for better visibility
+          },
+        })}
+      />
+      {/* Snackbar for error handling */}
+      <Snackbar
+        open={openSnackbar}
+        message="Failed to fetch training data."
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+      />
+    </div>
+  );
 }
 
 export default CalendarList;
