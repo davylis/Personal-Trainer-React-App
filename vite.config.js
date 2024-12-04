6008973d6acae24ad5431f9eab640ebd65ef76ea
Liisa Davydov <liisamuttonen@gmail.com>
2024-12-04 02:26:56 +0200

    Open you app's vite.config.js file and add the base property. The value is the name of your repository with leading and trailing slashes.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  //file and add the base property. The value is the name of your repository with leading and trailing slashes.
+  base: "/Personal-Trainer-React-App/",
   plugins: [react()],
-})
+});
